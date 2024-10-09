fc87e7595c730039b727015c3254df9981de3efa
mollah111 <jalammollah111@gmail.com>
2024-10-09 19:02:39 +0600

    Related if else

---
+++ script.js
@@ -0,0 +1,101 @@
+
+// let marks;
+
+// marks = -3
+
+// if(marks > 100 || marks < 0){
+//     console.log('Invalid marks')
+// }
+
+// else if(marks >= 80){
+//     console.log('The grade is: A+');
+// }
+
+// else if(marks >= 70){
+//     console.log('The grade is: A');
+// }
+
+// else if(marks >= 60){
+//     console.log('The grade is: A-');
+// }
+
+// else if(marks >= 50){
+//     console.log('The grade is: B');
+// }
+
+// else if(marks >= 40){
+//     console.log('The grade is: C');
+// }
+
+// else if(marks >= 33){
+//     console.log('The grade is: D');
+// }
+
+// else{
+//     console.log('The grade is: F')
+// }
+
+// let number;
+
+// number = 31
+
+// if(number % 2 == 0){
+//     console.log('This is an even number')
+// }
+
+// else{
+//     console.log('This is and odd number')
+// }
+
+console.log("Question 1. Write a Js program to find maximum between two numbers.");
+
+let number1 = 50;
+let number2 = 60;
+
+if(number1 > number2){
+    console.log("Number1 is the largest");
+}
+else{
+    console.log("Number2 is the largest");
+}
+
+console.log("Question 2. Write a Js program to check whether a number is negative, positive or zero.");
+
+// let number = 50;
+
+// if(number > 0){
+//     console.log("This number is positive");
+// }
+
+// else if(number < 0){
+//     console.log("This number is negative");
+// }
+
+// else{
+//     console.log("This is zero");
+// }
+
+console.log("Question 3. Write a Js program to check whether a number is divisible by 5 and 11 or not.");
+
+let number = 30;
+
+if(number % 5 == 0 && number % 11 ==0){
+    console.log("The number is divisible by 5 and 11");
+}
+
+else{
+    console.log("The number is not divisible by 5 and 11");
+}
+
+console.log("Question 4. Write a Js program to check whether a year is leap year or not.")
+
+let year = 2024;
+
+if(year % 4 == 0){
+    console.log("This is leap year")
+}
+
+else{
+    console.log("This is not leap year");
+}
+
